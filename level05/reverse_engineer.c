8302ce62aa1bf3fbf78d66d10c0689ef66bde01f
Mederis Frambourt <mframbou@student.42nice.fr>
2022-12-03 07:03:17 +0100

    reverse engineer gigachad

--- level05/reverse_engineer.c
+++ level05/reverse_engineer.c
@@ -56,6 +56,8 @@ int main(int argc, char **argv)
     pouet2 = 0xffffffff;
 
     /*
+        https://stackoverflow.com/questions/26783797/repnz-scas-assembly-instruction-specifics
+
         mov edx, buf
         mov eax, 0
         mov ecx, pouet2
@@ -67,7 +69,11 @@ int main(int argc, char **argv)
         cmp eax, 0x1f
         jb suce1
     */
-    if (strlen(buf) <= 0x1f) // copilot did this one so i hope its right
+    const char *p = buf;
+    while (*p++ != '\0')
+        pouet2--;
+    pouet2 = ~pouet2 - 1;
+    if (pouet2 < 0x1f)
         goto suce1;
 
     printf(buf);
@@ -80,18 +86,17 @@ int main()
 {
     char buf[100]; // esp+0x28
     int pouet = 0; // esp+0x8c
-    int pouet2 = 0xffffffff; // esp+0x1c
 
     fgets(buf, 100, stdin);
 
-    for (int i = 0; strlen(buf) <= 0x1f; i++, pouet2 = 0xffffffff)
+    for (int i = 0; strlen(buf) < 0x1f; i++)
     {
         // if (buf[i] <= 0x40)
         //     continue;
         // if (buf[i] > 0x5a)
         //     continue;
         // buf[i] ^= 0x20; // = lowercasify
-        
+
         if (buf[i] < 'A' || buf[i] > 'Z')
             continue;
 
