b9d3d5e764f2fe2c8c0dd3342f9cf8d83ce8f5d1
Mederis Frambourt <mframbou@student.42nice.fr>
2022-12-03 06:58:19 +0100

    v2 reverse engineer

--- level05/reverse_engineer.c
+++ level05/reverse_engineer.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
         mov eax, buf
         add eax, pouet
         movzx eax, byte ptr [eax]
-        cmp al, 0x40 (64)
+        cmp al, 0x40
         jle suce2
     */
     if (buf[pouet] <= 0x40)
@@ -30,10 +30,10 @@ int main(int argc, char **argv)
         mov eax, buf
         add eax, pouet
         movzx eax, byte ptr [eax]
-        cmp al, 0x5f (95)
+        cmp al, 0x5a
         jg suce2
     */
-    if (buf[pouet] > 0x5f)
+    if (buf[pouet] > 0x5a)
         goto suce2;
 
     /*
@@ -70,6 +70,34 @@ int main(int argc, char **argv)
     if (strlen(buf) <= 0x1f) // copilot did this one so i hope its right
         goto suce1;
 
+    printf(buf);
+    exit(0);
+}
+
+
+// V2
+int main()
+{
+    char buf[100]; // esp+0x28
+    int pouet = 0; // esp+0x8c
+    int pouet2 = 0xffffffff; // esp+0x1c
+
+    fgets(buf, 100, stdin);
+
+    for (int i = 0; strlen(buf) <= 0x1f; i++, pouet2 = 0xffffffff)
+    {
+        // if (buf[i] <= 0x40)
+        //     continue;
+        // if (buf[i] > 0x5a)
+        //     continue;
+        // buf[i] ^= 0x20; // = lowercasify
+        
+        if (buf[i] < 'A' || buf[i] > 'Z')
+            continue;
+
+        buf[i] -= ('A' - 'a');
+    }
+    
     printf(buf);
     exit(0);
 }
